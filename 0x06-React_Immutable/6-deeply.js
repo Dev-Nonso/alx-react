6f0d38293946e4579cddbe0e6e48f914169aa372
Dev-Nonso <chinonsoogbonnaya47@gmail.com>
2024-11-22 02:47:22 +0000

    Immutable.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,6 +1,4 @@
-import immutable from 'immutable';
-
-const { Map } = immutable;
+import { Map } from 'immutable';
 
 export default function mergeDeeplyElements(page1, page2) {
   return Map(page1).mergeDeep(Map(page2));
